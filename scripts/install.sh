183590d53e34c5f83d2437feb4a08b076231c106
Naveed Ahmad <naveed.ehmad@gmail.com>
2014-11-04 20:57:08 +0500

    more changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,22 @@
+echo "Changing shell…"
+chsh -s /bin/zsh
+
+echo "Cloning Sublime Text configuration…"
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3
+git clone https://github.com/soffes/sublime.git ~/Library/Application\ Support/Sublime\ Text\ 3
+
+echo "Hiding files…"
+SetFile -a "V" ~/Readme.markdown ~/LICENSE ~/Scripts
+
+echo "Excluding everything from git…"
+echo '*' >> ~/.git/info/exclude
+
 echo "Installing Homebrew…"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-./scripts/homebrew.sh
+./.dotfiles/scripts/homebrew.sh
+
+echo "Installing Ruby…"
+rbenv install 2.1.3
+rbenv global 2.1.3
+
+echo "Done."
